2ed28bf085e03dcbd6607971ff93ebacf3be677e
MohamedMahmoud01110 <mohamed.mahmoud14532@gmail.com>
2025-07-21 05:49:20 +0300

    add responsive design

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,14 @@
-import { Component, inject, Input } from '@angular/core';
+import { AfterViewInit, Component, inject, Input, ViewChild, ElementRef } from '@angular/core';
 import { ThemeService } from './../../services/theme.service';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../auth/services/auth.service';
+
+declare global {
+  interface Window {
+    bootstrap: any;
+  }
+}
 
 @Component({
   selector: 'app-navbar',
@@ -11,12 +17,13 @@ import { AuthService } from '../../auth/services/auth.service';
   standalone: true,
   imports: [RouterLink,RouterLinkActive,CommonModule],
 })
-export class NavbarComponent {
+export class NavbarComponent  {
   isDarkMode: boolean = false;
   theme : string ='';
   private readonly auth = inject(AuthService);
 
   @Input() layout!: string;
+  @ViewChild('navbarCollapse', { static: false }) navbarCollapse!: ElementRef;
 
 
   constructor(private themeService: ThemeService) {}
@@ -37,4 +44,36 @@ export class NavbarComponent {
     this.theme = this.theme === 'dark' ? 'light' : 'dark';
     this.themeService.setTheme(this.theme);
   }
+
+  onNavLinkClick() {
+    if (window.innerWidth < 992 && this.navbarCollapse) {
+      // Bootstrap 5 Collapse API
+      // @ts-ignore
+      const bsCollapse = window.bootstrap?.Collapse?.getOrCreateInstance
+        ? window.bootstrap.Collapse.getOrCreateInstance(this.navbarCollapse.nativeElement)
+        : null;
+      if (bsCollapse && bsCollapse.hide) {
+        bsCollapse.hide();
+      } else if (this.navbarCollapse.nativeElement.classList.contains('show')) {
+        this.navbarCollapse.nativeElement.classList.remove('show');
+      }
+    }
+  }
+
+  toggleNavbar() {
+    if (this.navbarCollapse) {
+      const bsCollapse = window.bootstrap?.Collapse?.getOrCreateInstance
+        ? window.bootstrap.Collapse.getOrCreateInstance(this.navbarCollapse.nativeElement)
+        : null;
+      if (bsCollapse) {
+        if (this.navbarCollapse.nativeElement.classList.contains('show')) {
+          bsCollapse.hide();
+        } else {
+          bsCollapse.show();
+        }
+      }
+    }
+  }
+
+
 }
